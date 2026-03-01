be5d413f768ac32f1e651645e2148f975c29ae0b
yusufsw1 <yusufsw390@gmail.com>
2026-03-01 10:24:49 +0700

    update 1

--- app/surprise/page.tsx
+++ app/surprise/page.tsx
@@ -20,13 +20,13 @@ export default function SurprisePage() {
       {showText && (
         <div className="relative z-20 flex flex-col items-center gap-3">
           {/* Big romantic title */}
-          <h1 className="font-cormorant italic font-light text-10xl md:text-9xl leading-tight">Happy Birthday</h1>
+          <h1 className="font-cormorant italic font-light text-xl md:text-9xl leading-tight">Happy Birthday</h1>
 
           {/* Name — big, gold-ish */}
-          <p className="font-cormorant italic font-light text-11xl md:text-9xl leading-tight">Sayangku </p>
+          <p className="font-cormorant italic font-light text-xl md:text-9xl leading-tight">Sayangku </p>
 
           {/* Subtitle — Raleway spaced */}
-          <p className="font-cormorant italic font-light text-10xl md:text-base text-gray-400 ">Barakallahu fii umrik, Semoga semua doa dan harapanmu terkabul ❤️. Milad Mubarak.</p>
+          <p className="font-cormorant italic font-light text-xl md:text-base text-gray-400 ">Barakallahu fii umrik, Semoga semua doa dan harapanmu terkabul ❤️. Milad Mubarak.</p>
         </div>
       )}
 
